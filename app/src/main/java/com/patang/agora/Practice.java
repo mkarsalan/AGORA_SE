8204970a11daeafa495c16559df67e34e481a7d9
Arsalan <arsalan@Arsalans-MacBook-Pro.local>
2018-04-10 18:13:05 +0500

    Initial commit

---
+++ app/src/main/java/com/patang/agora/Practice.java
@@ -0,0 +1,37 @@
+package com.patang.agora;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Practice extends AppCompatActivity{
+
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(Practice.this, Login.class);
+//                internt.putExtra("HELLO", result);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+    }
+
+}
