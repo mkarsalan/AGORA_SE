909220fbfa8acb16fee4a6c24a6e5af1ed6dd6d8
zainab saeed wattoo <zswattoo@gmail.com>
2018-04-16 15:01:35 +0500

    Zainab: Sign in done

--- app/src/main/java/com/patang/agora/Login.java
+++ app/src/main/java/com/patang/agora/Login.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,43 +18,41 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
-    private Button buttonRegister;
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private TextView textViewSignin;
 
-    private ProgressDialog progressDialog;
+    EditText editTextEmail;
+    EditText editTextPassword;
+    private FirebaseAuth mAuth;
 
-    private FirebaseAuth firebaseAuth;
+    ProgressBar progressbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        progressDialog = new ProgressDialog(this);
-
-        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        findViewById(R.id.buttonRegister).setOnClickListener(this);
 
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        progressbar = (ProgressBar) findViewById(R.id.progressbar);
 
-        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
+        mAuth = FirebaseAuth.getInstance();
 
-        buttonRegister.setOnClickListener(this);
-        textViewSignin.setOnClickListener(this);
+
+        //progressDialog = new ProgressDialog(this);
+        findViewById(R.id.textViewSignin).setOnClickListener(this);
     }
 
     private void registerUser(){
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
-        if (TextUtils.isEmpty(email)){
+       /* if (TextUtils.isEmpty(email)){
             Toast.makeText(this,"Please enter email", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -61,19 +60,55 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         if (TextUtils.isEmpty(password)){
             Toast.makeText(this,"Please enter password", Toast.LENGTH_SHORT).show();
             return;
+        }*/
+
+       //checking if email area is empty or not
+       if (email.isEmpty())
+       {
+           editTextEmail.setError("Please enter Email Address.");
+           editTextEmail.requestFocus();
+           return;
+       }
+
+       //Checking if the email address is valid or not
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            editTextEmail.setError("Please enter a valid Email Address.");
+            editTextEmail.requestFocus();
+            return;
         }
 
-        progressDialog.setMessage("Registering User...");
-        progressDialog.show();
+        //checking if password area is empty or not
+        if (password.isEmpty())
+        {
+            editTextPassword.setError("Please enter your Password.");
+            editTextPassword.requestFocus();
+            return;
+        }
 
-        firebaseAuth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+        //NOW REGISTERING THE USER
+        progressbar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressbar.setVisibility(View.GONE);
                         if (task.isSuccessful()){
-                            Toast.makeText(Login.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Registered Successfully.", Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(Login.this, "Could not Register", Toast.LENGTH_SHORT).show();
+
+                            //checking that the email is already registered
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException)
+                            {
+                                Toast.makeText(getApplicationContext(), "You are already registered.", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
                         }
                     }
                 });
@@ -81,8 +116,12 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View view){
-        if (view == buttonRegister){
+        if (view.getId() == R.id.buttonRegister){
             registerUser();
+        }
+        else if (view.getId() == R.id.textViewSignin)
+        {
+            startActivity(new Intent(this, signin.class));
         }
     }
 
